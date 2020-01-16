49c4ee72c3504db997dbdd93c06ae1793aba3ce9
ekater1na <ekaterina_l@tut.by>
2020-01-16 15:58:13 +0300

    feat: add navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
-
+import { PageComponent } from './page/page.component';
+ 
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: PageComponent, data: {
+    page: 'home'
+  }},
+  {path: 'about', component: PageComponent, data: {
+    page: 'about'
+  }},
+  {path: 'contact', component: PageComponent, data: {
+    page: 'contact'
+  }},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
+ 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentService } from '../shared/services/content.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-page',
@@ -6,15 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page.component.scss']
 })
 export class PageComponent implements OnInit {
-  page = {
-    title: 'Home',
-    subtitle: 'Welcome Home',
-    content: 'Some home content',
-    image: 'assets/bg00.jpg'
-  }
-  constructor() { }
+  page: any;
+
+  constructor(private route: ActivatedRoute,
+    private contentService: ContentService) { }
 
   ngOnInit() {
+    const pageData = this.route.snapshot.data['page'];
+    this.page = this.contentService.pages[pageData];
   }
-
 }
