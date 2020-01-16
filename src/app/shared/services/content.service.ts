ea80e7cccb8d51454e96812496fb4a524745164b
ekater1na <ekaterina_l@tut.by>
2020-01-16 17:10:30 +0300

    refactor: refactor code with linter

--- src/app/shared/services/content.service.ts
+++ src/app/shared/services/content.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ContentService {
-  pages: Object = {
+  pages: object = {
     home: {title: 'Home', subtitle: 'Welcome Home!', content: 'Some home content.', image: 'assets/bg00.jpg'},
     about: {title: 'About', subtitle: 'About Us', content: 'Some content about us.', image: 'assets/bg01.jpg'},
     contact: {title: 'Contact', subtitle: 'Contact Us', content: 'How to contact us.', image: 'assets/bg02.jpg'}
